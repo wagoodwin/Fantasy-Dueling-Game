37876a0bbf32e7f260d39a28c77ba5647b867c7d
Walter <wagoodw1@asu.edu>
2022-06-04 21:15:48 -0700

    added basic turn-based component

--- main.cpp
+++ main.cpp
@@ -22,247 +22,11 @@
 // // ends the game and gives the player money or something, etc
 
 
-
-
-// // could use ineritance later on to make Player and Computer derived from some sort of generic "dueler" class
-
-// class Player
-// {
-// private:
-//     int m_hp {};
-//     int m_action {};
-
-// public:
-
-//     Player(int hp = 5): m_hp{hp} 
-//     {
-
-//     }
-
-//     int attack(const int& attackDamage)
-//     {
-//         return attackDamage;
-//     }
-
-//     int getHP() const // const because getHP shouldn't alter hp
-//     {
-//         return m_hp;
-//     }
-
-//     void takeHP(int damage)
-//     {
-//         m_hp = m_hp - damage;
-//     }
-
-//     void giveHP(int healing)
-//     {
-//         m_hp = m_hp + healing;
-//     }
-
-
-// };
-
-
-
-// class Computer
-// {
-// private:
-//     int m_hp {};
-// public:
-
-//     Computer(int hp = 5): m_hp{hp}
-//     {
-
-//     }
-
-//     // int attack(const int& attackDamage)
-//     // {
-//     //     return attackDamage;
-//     // }
-
-//     int attack(const int& attackDamage)
-//     {
-//         return attackDamage;
-//     }
-
-//     int getHP() const // const because getHP shouldn't alter hp
-//     {
-//         return m_hp;
-//     }
-
-//     void takeHP(int damage)
-//     {
-//         m_hp = m_hp - damage;
-//     }
-
-//     void giveHP(int healing)
-//     {
-//         m_hp = m_hp + healing;
-//     }
- 
-
-
-
-// // void action(int m_action)
-// // {
-// //     switch (m_action)
-// //     {
-// //         case 1: std::cout << "attack"; 
-// //         break;
-// //         case 2: std::cout << "defend";
-// //         break;
-
-// //         default:
-// //         std::cout << "ooga";
-// //         break;
-// //     }
-// // }
-
-// };
-
-
-
-// int healthCheck(Player& player, Computer& computer)
-// {
-
-//     if (player.getHP() > 0 && computer.getHP() > 0)
-//     {
-//         return 1;
-//     }
-//     else if (player.getHP() > 0 && computer.getHP() <= 0)
-//     {
-//         return 2;
-//     }
-//     else if (player.getHP() <= 0 && computer.getHP() > 0)
-//     {
-//         return 3;
-//     }
-//     else if (player.getHP() <= 0 && computer.getHP() <= 0)
-//     {
-//         return 4;
-//     }
-
-// }
-
-
-// int gameStatus(int healthCheck, Player& player, Computer& computer)
-// {
-//     switch(healthCheck)
-//     {
-//         case 1:
-//             std::cout << "The battle continues." << "\n";
-//             std::cout << "Player HP: " << player.getHP() << "\n";
-//             std::cout << "Computer HP: " << computer.getHP() << "\n";
-//             return 1;
-//         break;
-//         case 2:
-//             std::cout << "The monster has been slain";
-//             // add looting function here in future 
-//             // Will add money to player object here with addmoney()
-//             return 2;
-//         break;
-//         case 3:
-//             std::cout << "The player has been slain";
-//             return 3;
-//         break;
-//         case 4:
-//             std::cout << "Both duelers have been slain";
-//             return 4;
-//         break;
-
-//         default:
-//             std::cout << "error: received an int not 1,2,3,4";
-//             return 5;
-//         break;
-//     }
-
-// }
-
-
-// int exchangeAttack(Computer& computer, Player& player)
-// {
-
-//     return 0;
-// }
-
-// void playerTurn(Player& player, Computer& computer)
-// {
-//     computer.takeHP(1);
-
-// }
-
-// void computerTurn(Player& player, Computer& computer)
-// {
-//     player.takeHP(1);
-// }
-
-
-
-
-// // so maybe make this a bool and make it return true or false based on the result of the HP check.
-// // it should go (human turn - hpCheck - computer turn - hpCheck) as a pattern
-// int playDuelingGame(Player& player, Computer& computer) 
-// {
-
-//     // if (player.getHP() <= 0)
-//     // {
-//     //     return false;
-//     // }
-
-//     int gameCondition{};
-
-//     playerTurn(player, computer);
-
-//     gameCondition = healthCheck(player,computer);
-//     gameStatus(gameCondition, player, computer);
-
-//     computerTurn(player, computer);
-
-//     gameCondition = healthCheck(player,computer);
-//     gameStatus(gameCondition, player, computer);
-
-//     // computerTurn(player, computer);
-
-//     // gameCondition = healthCheck(player,computer);
-//     // gameStatus(gameCondition, player, computer);
-
-
-// }
-
-
-// int main()
-// {
-//     // initialize player and computer
-//     Player player {};
-//     Computer computer {};
-
-//     int gameCondition{1};
-
-//     // game should continue while the player is still alive
-//     while(gameStatus(gameCondition, player, computer) == 1)
-//     {
-//         playerTurn(player, computer);
-//         // attempt at checking the game status after each turn
-//         gameCondition = healthCheck(player,computer); 
-//         gameStatus(gameCondition, player, computer);
-
-//         computerTurn(player, computer);
-//         gameCondition = healthCheck(player,computer);
-//         gameStatus(gameCondition, player, computer);
-//     }
-
-//     std::cout << "The battle is over" << "\n";
-
-
-//     return 0;
-// }
-
 class Dueler
 {
 private:
     int m_hp {};
     int m_action {};
-
 public:
 
     Dueler(int hp = 5): m_hp{hp} 
@@ -275,7 +39,7 @@ public:
         return attackDamage;
     }
 
-    int getHP() const // const because getHP shouldn't alter hp
+    int showHP() const // const because getHP shouldn't alter hp
     {
         return m_hp;
     }
@@ -285,56 +49,47 @@ public:
         m_hp = m_hp - damage;
     }
 
-    void giveHP(int healing)
+    void heal(int healing)
     {
+        std::cout << "oogabooga" << "\n";
         m_hp = m_hp + healing;
     }
 
-
 };
 
-class Player
+// we want to inherit basic stuff like getters and setters, but we don't want to inherit 
+// stuff like HP, as HP will be different between duelers.
+class Player: public Dueler
 {
 private:
     int m_hp {};
     int m_action {};
-
 public:
 
     Player(int hp = 5): m_hp{hp} 
     {
 
     }
+    // very bizzarre scenes: when we include this function, the player doesn't actually heal.
+    // The player does heal when we don't include it-- that is, player inherits heal() from 
+    // Dealer and it works, but player's own heal function doesn't work. Big thing we gotta resolve. 
 
-    int attack(const int& attackDamage)
+    void heal(int healing)
     {
-        return attackDamage;
-    }
-
-    int getHP() const // const because getHP shouldn't alter hp
-    {
-        return m_hp;
-    }
-
-    void takeHP(int damage)
-    {
-        m_hp = m_hp - damage;
-    }
-
-    void giveHP(int healing)
-    {
-        m_hp = m_hp + healing;
+        std::cout << "The player is casting a healing spell to add "
+        << healing << " hp" << "\n"; 
+        m_hp = m_hp + healing; // this line insn't working
     }
 
 
 };
 
 
-
-class Computer
+class Computer: public Dueler 
 {
 private:
     int m_hp {};
+    int m_action {};
 public:
 
     Computer(int hp = 5): m_hp{hp}
@@ -342,49 +97,38 @@ public:
 
     }
 
-    int attack(const int& attackDamage)
-    {
-        return attackDamage;
-    }
-
-    int getHP() const // const because getHP shouldn't alter hp
-    {
-        return m_hp;
-    }
-
-    void takeHP(int damage)
-    {
-        m_hp = m_hp - damage;
-    }
-
-    void giveHP(int healing)
-    {
-        m_hp = m_hp + healing;
-    }
-
-
 };
 
 
+void playerTurn(Player& player, Computer& computer)
+{
+    computer.takeHP(1);
+
+}
+
+void computerTurn(Player& player, Computer& computer)
+{
+    player.takeHP(1);
+}
 
 
 
 int healthCheck(Player& player, Computer& computer)
 {
 
-    if (player.getHP() > 0 && computer.getHP() > 0)
+    if (player.showHP() > 0 && computer.showHP() > 0)
     {
         return 1;
     }
-    else if (player.getHP() > 0 && computer.getHP() <= 0)
+    else if (player.showHP() > 0 && computer.showHP() <= 0)
     {
         return 2;
     }
-    else if (player.getHP() <= 0 && computer.getHP() > 0)
+    else if (player.showHP() <= 0 && computer.showHP() > 0)
     {
         return 3;
     }
-    else if (player.getHP() <= 0 && computer.getHP() <= 0)
+    else if (player.showHP() <= 0 && computer.showHP() <= 0)
     {
         return 4;
     }
@@ -401,43 +145,32 @@ int gameStatus(int healthCheck, Player& player, Computer& computer)
     switch(healthCheck)
     {
         case 1:
-            std::cout << "The battle continues." << "\n";
-            std::cout << "Player HP: " << player.getHP() << "\n";
-            std::cout << "Computer HP: " << computer.getHP() << "\n";
+            std::cout << "\n";
+            std::cout << "Player HP: " << player.showHP() << "\n";
+            std::cout << "Computer HP: " << computer.showHP() << "\n";
+            std::cout << "\n";
             return 1;
-        break;
+            break;
         case 2:
-            std::cout << "The monster has been slain";
+            std::cout << "The monster has been slain" << "\n";
             // add looting function here in future 
             // Will add money to player object here with addmoney()
             return 2;
-        break;
+            break;
         case 3:
-            std::cout << "The player has been slain";
+            std::cout << "The player has been slain" << "\n";
             return 3;
-        break;
+            break;
         case 4:
-            std::cout << "Both duelers have been slain";
+            std::cout << "Both duelers have been slain" << "\n";
             return 4;
-        break;
-
+            break;
         default:
-            std::cout << "error: received an int not 1,2,3,4";
+            std::cout << "error: received an int not 1,2,3,4" << "\n";
             return 5;
-        break;
+            break;
     }
 
-}
-
-void playerTurn(Player& player, Computer& computer)
-{
-    computer.takeHP(1);
-
-}
-
-void computerTurn(Player& player, Computer& computer)
-{
-    player.takeHP(1);
 }
 
 int main()
@@ -445,20 +178,36 @@ int main()
     // initialize player and computer
     Player player {};
     Computer computer {};
+    char input;
 
     int gameCondition{1};
 
     // game should continue while the player is still alive
     while(gameStatus(gameCondition, player, computer) == 1)
     {
-        playerTurn(player, computer);
-        // attempt at checking the game status after each turn
-        gameCondition = healthCheck(player,computer); 
-        gameStatus(gameCondition, player, computer);
+        std::cout << "Choose your action. Press 'a' to attack," 
+        << " 'd' to dodge, or 'b' to block." << "\n";
+        std::cin >> input;
 
+        switch(input)
+        {
+            case 'a': playerTurn(player, computer);  break;
+            case 'd': std::cout << "dodge";          break;
+            case 'b': player.heal(5);                break; // for some reason, player's HP isn't increasing. Not an issue with switch()
+
+            default: 
+                std::cout << "That command isn't recognized. ";
+                std::cout << "Your action defaults to attack.";
+                playerTurn(player, computer);
+            break;
+        }
+
+        // check the game status after each turn
+        gameCondition = healthCheck(player,computer); 
         if(gameStatus(gameCondition, player, computer) != 1)
             break;
 
+        std::cout << "Computer's turn:";
         computerTurn(player, computer);
         // gameCondition = healthCheck(player,computer);
         // gameStatus(gameCondition, player, computer);
@@ -469,3 +218,16 @@ int main()
 
     return 0;
 }
+
+//TODO: add in a turn component. The player should be prompted to attack, and then 
+// stuff should continue. Also, you have some overridden functions, so implementing some
+// virtual functions might be a good exercise. Also, shoehoern in const and constexpr for variable initializtion.
+// other stuff to include: enums, arrays
+
+
+// TODO: figure out why player.heal() doesn't add hp to the player. The turn aspect is integrated
+// now, so the next step is to fold in these different abilities into each class. We need to figure out 
+// a good way to have these methods (like attack) interact with the environment.
+
+// After that, make multiple weapons. Make a class called Weapon. Then derive stuff (swords, maces)
+// from Weapon.Also, using examples from learncpp.com, implement some sort of virtual function mechanic.
